d930fc4e87dd47a6b15167ec247f8427651cf386
FehDx01 <lessa.fernando01@gmail.com>
2023-11-21 19:17:20 -0300

    exercícios de arrays

---
+++ exercicio1/script.js
@@ -0,0 +1,7 @@
+/* const numeros = [10, 23, 45, 18]
+const frases = ["maça", "banana", "chevette"]
+const misto = [14, "panela", true]
+
+console.log(numeros)
+console.log(frases)
+console.log(misto) */
---
+++ exercicio2/script.js
@@ -0,0 +1,42 @@
+//a 
+
+/* function arrayParametro(numeros, frases, misto) {
+
+    const array1 = [10, 23, 45, 18]
+    const array2 = ["maça", "banana", "chevette"]
+    const array3 = [14, "panela", true]
+    
+numeros = array1
+frases = array2
+misto = array3
+
+    console.log(numeros.length)
+    console.log(frases.length)
+    console.log(misto.length)
+
+}
+arrayParametro() */
+
+//b e c
+
+/* function arrayParametro(numeros, frases, misto) {
+
+    const array1 = [10, 23, 45, 18]
+    const array2 = ["maça", "banana", "chevette"]
+    const array3 = [14, "panela", true]
+    
+numeros = array1
+frases = array2
+misto = array3
+
+    verificaArray = array1.includes(10)
+    verificaArray2 = array3.includes("pamela")
+
+    console.log(verificaArray)
+    console.log(verificaArray2)
+
+}
+arrayParametro()
+ */
+
+
